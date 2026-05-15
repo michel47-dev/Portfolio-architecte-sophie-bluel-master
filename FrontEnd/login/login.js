b2bc8339aad24ae460f2bb95514d8bd782f0556e
Guestin michel <michel47390@outlook.fr>
2026-05-15 21:41:05 +0200

    feat: "Ajout du système de connexion"

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -1,5 +1,39 @@
 const loginForm = document.getElementById("login-form");
 
-loginForm.addEventListener("submit", function(event){
+loginForm.addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    method: "POST",
+
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  });
+
+  if (response.ok) {
+
+    const data = await response.json();
+
+    localStorage.setItem("token", data.token);
+
+    window.location.href = "index.html";
+
+  } else {
+
+    const erreurConnection = document.getElementById ("error-message");
+    erreurConnection.innerText = "Email ou mot de passe incorrect";
+
+  }
 
 });
+  
+
