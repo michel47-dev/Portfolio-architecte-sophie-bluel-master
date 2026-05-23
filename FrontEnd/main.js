2587e717cf9f71143cf0f3c2c44a3fe06eda475e
Guestin michel <michel47390@outlook.fr>
2026-05-23 15:50:40 +0200

    feat:" ajout et suppression de projet avec modal"

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -25,6 +25,8 @@ if (token){
 
 }
 
+let works= [];
+
 async function recupereTravaux() {
   const response = await fetch("http://localhost:5678/api/works");
   const data = await response.json();
@@ -89,12 +91,13 @@ boutonTous.addEventListener("click", () => {
 }
 
 async function initialiserProjet() {
-  const works = await recupereTravaux();
+  works = await recupereTravaux();
   const categories = await recupereCategorie();
 
   afficherProjets(works);
   afficherProjetsModal(works);
   afficherCategorie(categories, works);
+  afficherCategorieSelect(categories);
   console.log(works, categories)
 }
 
@@ -137,10 +140,17 @@ function afficherProjetsModal (projets) {
      const corbeille = document.createElement("i");
     corbeille.classList.add("fa-solid", "fa-trash-can");
 
-    corbeille.addEventListener("click", function(){
+    corbeille.addEventListener("click",async function(){
     console.log(work.id);
-    
-      modalItem.remove();
+   const response = await fetch(`http://localhost:5678/api/works/${work.id}`, {
+  method: "DELETE",
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
+     if (response.ok) {
+  modalItem.remove();
+}
     })
 
     img.src = work.imageUrl;
@@ -173,7 +183,100 @@ const retourModal = document.querySelector(".retour-modal");
 retourModal.addEventListener("click", function(){
   vueGallery.style.display = "block";
   ajoutPhoto.style.display = "none";
+});
+
+
+const inputImage = document.getElementById("image");
+const previewImage = document.querySelector(".preview-photo");
+const iconImage = document.querySelector(".upload-photo i");
+const labelImage = document.querySelector(".upload-photo label");
+const texteImage = document.querySelector(".upload-photo p");
+const btnValider = document.querySelector (".btn-valider");
+const titleInput= document.getElementById("title");
+const categorySelect = document.getElementById("category");
+
+function verifierFormulaire(){
+  const imageOk =inputImage.files.length > 0 ;
+  const titleOk = titleInput.value.trim() !== "";
+  const categoryOk = categorySelect.value !== "";
+  if (imageOk && titleOk && categoryOk){
+    btnValider.style.backgroundColor =  "#1D6154";
+    btnValider.disabled = false;
+  }
+  else {
+    btnValider.disabled =true;
+  }
+};
+verifierFormulaire();
+
+titleInput.addEventListener("input", function (){
+  verifierFormulaire();
 })
+categorySelect.addEventListener("change", function (){
+  verifierFormulaire();
+})
+
+
+inputImage.addEventListener("change", function(){
+   previewImage.src = URL.createObjectURL(inputImage.files[0]);
+   previewImage.style.display = "block";
+   iconImage.style.display = "none";
+labelImage.style.display = "none";
+texteImage.style.display = "none";
+verifierFormulaire();
+
+});
+
+
+const formAjoutPhoto = document.querySelector(".ajout-photo form");
+
+formAjoutPhoto.addEventListener("submit",async function (event){
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", document.getElementById("title").value);
+    formData.append("image", inputImage.files[0]);
+    formData.append("category", document.getElementById("category").value);
+    const response = await fetch("http://localhost:5678/api/works",{
+        method: "POST",
+
+        headers: {
+          Authorization:`Bearer ${token}`
+        },
+
+        body: formData,
+    });
+    console.log(response);
+    const newWork = await response .json();
+    console.log(newWork);
+    works.push(newWork);
+    afficherProjets(works);
+    afficherProjetsModal(works);
+    formAjoutPhoto.reset();
+
+    previewImage.style.display = "none";
+previewImage.src = "";
+
+iconImage.style.display = "block";
+labelImage.style.display = "block";
+texteImage.style.display = "block";
+ajoutPhoto.style.display = "none";
+vueGallery.style.display = "block";
+    
+  });
+
+  function afficherCategorieSelect(categories){
+    const selectCategory = document.getElementById("category");
+
+    categories.forEach(categorie => {
+      const option = document.createElement("option");
+
+      option.value = categorie.id;
+      option.innerText = categorie.name;
+
+      selectCategory.appendChild(option);
+    })
+  }
 
 
 
